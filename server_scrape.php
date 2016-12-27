de2a63647f5d62605ead31ec89291d1bdb0ab09f
Marcus Therkildsen <marcustherkildsen@gmail.com>
2016-12-28 00:42:59 +0100

    Able to retrieve placement array from JSON data
    Combined the valid faust numbers into the a request for the JSON data.
    Retrieved the placement array.

--- server_scrape.php
+++ server_scrape.php
@@ -26,13 +26,17 @@ function DOMinnerHTML(DOMNode $element)
 
 
 // Static address, just for testing
-$query = 'https://www.aakb.dk/search/ting/harry%20potter';
+$query = 'https://www.aakb.dk/search/ting/snowden';//'https://www.aakb.dk/search/ting/harry%20potter';
 
 $dom = new DOMDocument('1.0');
 $classname = "search-result--heading-type";
-@$dom->loadHTMLFile($query);
+@$dom->loadHTMLFile($query); // Returns true for success and false otherwise. Should make a check.
 $nodes = array();
 $nodes = $dom->getElementsByTagName("span");
+$arr_faust = array();
+
+$part_url = "/ding_availability/holdings/";
+
 
 foreach ($nodes as $element)
 {
@@ -40,22 +44,35 @@ foreach ($nodes as $element)
    if ($classy == $classname)
    {
 
+    // Out starting point for scraping
     $parent_element = $element->parentNode->parentNode->parentNode;
+    $first_children = $parent_element->childNodes->item(2)->childNodes;
 
     // Here we should make a check for "870970-basis" (12chars) og "775100-katalog" (14chars) which are the only two cases where the item "might" be available at the library.
     // As a start, I will simply check for 870970 and 775100
-    $in_faust = substr($parent_element->childNodes->item(2)->childNodes->item(1)->childNodes->item(1)->childNodes->item(0)->getAttribute('href'), 17, 6);
+    $in_faust = substr($first_children->item(1)->childNodes->item(1)->childNodes->item(0)->getAttribute('href'), 17, 6);
     if ($in_faust == 870970 || $in_faust == 775100)
     {
 
       // Faust
-      $faust = substr($parent_element->childNodes->item(2)->childNodes->item(1)->childNodes->item(1)->childNodes->item(0)->getAttribute('href'), -8);
+      $faust = substr($first_children->item(1)->childNodes->item(1)->childNodes->item(0)->getAttribute('href'), -8);
+
+      // Note: use DOMinnerHTML($element) if you want hyperlink
+      // use ->textContent if you simply want the text.
 
       // Title
-      $title = $parent_element->childNodes->item(2)->childNodes->item(1)->childNodes->item(1)->childNodes->item(0)->textContent;
+      $temp_title = $first_children->item(1)->childNodes->item(1)->childNodes->item(0);
+      if (!is_null($temp_title))
+      {
+        $title = $temp_title->textContent;
+      }
+      else
+      {
+        $title = 'Ukendt';//null; // Insert 'Ukendt' in JS
+      }
 
-      // Author
-      $author = DOMinnerHTML($parent_element->childNodes->item(2)->childNodes->item(2)->nextSibling);
+      // Author (will show only release year if no author available) but that might very well change when/if they ever finish the new system. 
+      $author = $first_children->item(2)->nextSibling->textContent;
 
       // Thumb
       $temp_thumb = $parent_element->childNodes->item(1)->getElementsByTagName('img')->item(0);
@@ -65,11 +82,12 @@ foreach ($nodes as $element)
       }
       else
       {
-        $thumb = 'No thumbnail';
+        $thumb = 'No thumbnail';//null; // Insert http://www.dokk1nfo.dk/img/no_image.jpg' in JS
       }
 
-      echo $in_faust;
-      echo '<br>';
+      // http://php.net/manual/en/language.operators.string.php
+      $part_url.="{$faust},";
+
       echo $faust;
       echo '<br>';
       echo $thumb;
@@ -79,8 +97,71 @@ foreach ($nodes as $element)
       echo $title;
       echo '<br>';
       echo '<br>';
+
+      array_push($arr_faust, $faust);
     }
   }
+}
+
+echo $part_url;
+echo '<br>';
+// Remove trailing comma http://stackoverflow.com/a/5593009/5241172
+$part_url = rtrim($part_url, ",");
+echo $part_url;
+
+echo '<br>';
+
+/*
+foreach($arr_faust as $real_faust){
+  echo $real_faust;
+  echo '<br>';
+}
+*/
+
+
+// Get the json object http://stackoverflow.com/a/15617547/5241172
+ini_set("allow_url_fopen", 1);
+$json = file_get_contents("https://www.aakb.dk/{$part_url}");
+echo "https://www.aakb.dk/{$part_url}";
+echo '<br>';
+echo $json;
+// Return as array http://stackoverflow.com/a/6815562/5241172
+$obj = json_decode($json, true);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+foreach($arr_faust as $real_faust){
+  
+  
+  // Count number of array entries (each count is a library)
+  $lib_count = count($obj[$real_faust]['holdings']);
+  for($i=0;$i<$lib_count;$i++){
+  
+    $placement_count = count($obj[$real_faust]['holdings'][$i]['placement']);
+
+    echo "[";
+    for($k=0;$k<$placement_count;$k++){
+
+      echo $obj[$real_faust]['holdings'][$i]['placement'][$k];
+
+
+      if($k!=($placement_count-1)){
+        echo ",";
+      }
+
+
+    }
+
+    echo "]";
+  
+
+    echo '<br>';
+  }
+  /*
+  echo $obj[$real_faust]['holdings'][0]['placement'][0];
+  echo '<br>';
+*/
 }
 
 ?>
